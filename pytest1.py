b4d0e019b889e87e42847ebf6173092842c668aa
Baxter Waltermire <bax@Baxters-MacBook-Pro.local>
2014-08-09 20:27:09 -0400

    Added two more functions.

--- pytest1.py
+++ pytest1.py
@@ -3,7 +3,17 @@ def double_it(num):
     print "Input: %d  Doubled: %d" % (num, 2 * num)
 
 
+def triple_it(num):
+    print "Input: %d  Tripled: %d" % (num, 3 * num)
+
+
+def bigger_it(num):
+    factor = 9876543210123456789
+    print "Input: %d  Bigger: %d" % (num, factor * num)
+
+
 if __name__ == "__main__":
     print "testing double"
     double_it(10)
     double_it(500)
+
